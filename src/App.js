74f87d4e7da9a22cfaa62b9969bca7db73bc15f7
Valentin Ageev <sazuru@gmail.com>
2020-05-09 23:23:16 +0300

    Added search ability

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 require('dotenv').config();
 
@@ -6,12 +7,35 @@ const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function App() {
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
-    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+    getPhotos();
+  }, [page]);
+
+  const getPhotos = () => {
+    let apiUrl = `https://api.unsplash.com/photos?`;
+    if (query)
+      apiUrl = `https://api.unsplash.com/search/photos?&query=${query}`;
+
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+    fetch(apiUrl)
       .then((response) => response.json())
-      .then(setImages);
-  }, []);
+      .then((data) => {
+        const imagesFromApi = data.results ?? data;
+
+        if (page === 1) setImages(imagesFromApi);
+        setImages((images) => [...images, ...imagesFromApi]);
+      });
+  };
+
+  const searchPhotos = (e) => {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
+  };
 
   if (!accessKey) {
     return (
@@ -25,18 +49,36 @@ export default function App() {
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
         <button>Search</button>
       </form>
-
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
