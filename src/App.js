3d95b8f34e708e1471171283143aadd5349d5917
Valentin Ageev <sazuru@gmail.com>
2020-05-09 16:16:13 +0300

    Added unsplash images urls and descriptions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 require('dotenv').config();
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 export default function App() {
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
-    fetch(
-      `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
-    )
+    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+      .then(setImages);
   }, []);
+
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/developers" className="error">
+        Grab Your Key Here!
+      </a>
+    );
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -22,9 +31,9 @@ export default function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
